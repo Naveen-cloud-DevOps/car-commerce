04b7e0f0bd01819c0d7d0e6b6471728bea2b0382
Naveen <venkata.cloud16@gmail.com>
2026-08-28 15:48:41 +0530

    car-ecommerce

---
+++ app.js
@@ -0,0 +1,61 @@
+const cars = [
+    {
+        id: 1,
+        name: "BMW M4 Competition",
+        category: "sports",
+        price: 9500000,
+        year: 2025,
+        fuel: "Petrol",
+        image: "images/car1.jpg"
+    },
+
+    {
+        id: 2,
+        name: "Mercedes-Benz C-Class",
+        category: "sedan",
+        price: 6200000,
+        year: 2025,
+        fuel: "Petrol",
+        image: "images/car2.jpg"
+    },
+
+    {
+        id: 3,
+        name: "Audi Q8",
+        category: "suv",
+        price: 8800000,
+        year: 2025,
+        fuel: "Diesel",
+        image: "images/car3.jpg"
+    },
+
+    {
+        id: 4,
+        name: "Tesla Model S",
+        category: "electric",
+        price: 7200000,
+        year: 2025,
+        fuel: "Electric",
+        image: "images/car4.jpg"
+    },
+
+    {
+        id: 5,
+        name: "Range Rover Sport",
+        category: "suv",
+        price: 12500000,
+        year: 2025,
+        fuel: "Diesel",
+        image: "images/car5.jpg"
+    },
+
+    {
+        id: 6,
+        name: "Porsche 911 Carrera",
+        category: "sports",
+        price: 18500000,
+        year: 2025,
+        fuel: "Petrol",
+        image: "images/car6.jpg"
+    }
+];
